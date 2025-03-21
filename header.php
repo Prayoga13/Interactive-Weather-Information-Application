60f90d1e7c7d3b26439fcc2afeaa69bbb4cf7f59
Prayoga13 <76443196+Prayoga13@users.noreply.github.com>
2025-03-21 10:24:08 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather Station</title>
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="header.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- JS -->
+    <script src="js/script.js" defer></script>
+
+    <!-- Box Icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- Google Icons -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search" />
+</head>
+
+<body>
+
+    <section class="header">
+        <input type="checkbox" name="" id="chek">
+        <label for="chek"><i class="fa-solid fa-bars"></i></label>
+        <h1 class="logo" style="color:black;">Weather Monitoring</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="simpleApp.php">Simple App</a>
+            <a href="fullApp.php">Full App</a>
+        </nav>
+        <a href="simpleApp.php" class="btn">Selengkapnya</a>
+    </section>
